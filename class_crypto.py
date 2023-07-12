a32a0340d11953ebbca9022592c7c316e043ea07
Wojciech Mycek <66401382+WojciechMycek@users.noreply.github.com>
2023-07-12 15:19:53 +0000

    Defined class for multi encryption

--- class_crypto.py
+++ class_crypto.py
@@ -1,7 +1,9 @@
+import csv
 import cryptography
 from cryptography.fernet import Fernet
+from cryptography.fernet import MultiFernet
 
-class CSVFileHandler:
+class CSVFileHandlerFenter:
     def __init__(self, key_file_path):
         self.key_file_path = key_file_path
         self.key = self.load_key()
@@ -29,3 +31,62 @@ class CSVFileHandler:
         decrypted = f.decrypt(encrypted)
         with open(output_file_path, 'wb') as decrypted_file:
             decrypted_file.write(decrypted)
+
+class MultiFernetHandler:
+    def __init__(self, keys):
+        self.fernets = [Fernet(key) for key in keys]
+
+    def encrypt(self, message):
+        encrypted_message = message
+        for fernet in self.fernets:
+            encrypted_message = fernet.encrypt(encrypted_message)
+        return encrypted_message
+
+    def decrypt(self, token):
+        decrypted_token = token
+        for fernet in reversed(self.fernets):
+            decrypted_token = fernet.decrypt(decrypted_token)
+        return decrypted_token
+
+    def rotate(self, token):
+        rotated_token = token
+        for fernet in self.fernets:
+            rotated_token = fernet.encrypt(rotated_token)
+        return rotated_token
+
+    @staticmethod
+    def generate_keys(num_keys):
+        keys = []
+        for _ in range(num_keys):
+            keys.append(Fernet.generate_key())
+        return keys
+
+
+class CSVFileHandlerMulti:
+    def __init__(self, key_file_path, num_keys):
+        self.key_file_path = key_file_path
+        self.keys = MultiFernetHandler.generate_keys(num_keys)
+        self.handler = MultiFernetHandler(self.keys)
+
+    def load_keys(self):
+        with open(self.key_file_path, 'wb') as key_file:
+            for key in self.keys:
+                key_file.write(key + b'\n')
+
+    def encrypt_csv(self, input_file_path, output_file_path):
+        self.load_keys()
+        with open(input_file_path, 'r') as input_file, open(output_file_path, 'w', newline='') as output_file:
+            reader = csv.reader(input_file)
+            writer = csv.writer(output_file)
+            for row in reader:
+                encrypted_row = [self.handler.encrypt(field.encode()) for field in row]
+                writer.writerow(encrypted_row)
+
+    def decrypt_csv(self, input_file_path, output_file_path):
+        self.load_keys()
+        with open(input_file_path, 'r') as input_file, open(output_file_path, 'w', newline='') as output_file:
+            reader = csv.reader(input_file)
+            writer = csv.writer(output_file)
+            for row in reader:
+                decrypted_row = [self.handler.decrypt(field.encode()).decode() for field in row]
+                writer.writerow(decrypted_row)
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from class_crypto import CSVFileHandler
+from class_crypto import CSVFileHandlerFenter
+from class_crypto import CSVFileHandlerMulti
 
-# Initial object
+print(" Hello. You can choose in my programm to define which method to encrypt and decrypt you can use: 1. Library Cryptography: Fernet (symmetric encryption)")
 
-file_handler = CSVFileHandler('mykey.key')
+decision = int(input())
 
-# Encrypting file
-file_handler.encrypt_csv('date.csv', 'enc_date.csv')
+if decision == 1:
+    # Initial object
 
-# Decrypting file
-file_handler.decrypt_csv('enc_date.csv', 'dec_grades.csv')
+    file_handler = CSVFileHandlerFenter('mykey.key')
+
+    # Encrypting file
+    file_handler.encrypt_csv('date.csv', 'enc_date.csv')
+
+    # Decrypting file
+    file_handler.decrypt_csv('enc_date.csv', 'dec_grades.csv')
+
+elif decision == 2:
+    print("Multi Encryption!!!")
+
+    file_handler = CSVFileHandlerMulti('mykeys_multi.key', 2)
+    file_handler.encrypt_csv('date.csv', 'enc_date_multi.csv')
