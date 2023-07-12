3a5970599d570d02f9e238abd4489316e5f61c1c
Wojciech Mycek <66401382+WojciechMycek@users.noreply.github.com>
2023-07-12 14:29:36 +0000

    define WA with cryptography library

--- crypto.py
+++ crypto.py
@@ -1,1 +1,31 @@
+import cryptography
+from cryptography.fernet import Fernet
 
+#creating key
+key = Fernet.generate_key()
+
+with open('mykey.key', 'wb') as mykey:
+    mykey.write(key)
+
+print(key)
+
+#defined our excel to hide information
+
+f = Fernet(key)
+
+with open('date.csv', 'rb') as original_file:
+    original = original_file.read()
+
+encrypted = f.encrypt(original)
+
+with open ('enc_date.csv', 'wb') as encrypted_file:
+    encrypted_file.write(encrypted)
+
+#reading information from encrypted file
+with open('enc_date.csv', 'rb') as encrypted_file:
+    encrypted = encrypted_file.read()
+
+decrypted = f.decrypt(encrypted)
+
+with open('dec_grades.csv', 'wb') as decrypted_file:
+    decrypted_file.write(decrypted)
